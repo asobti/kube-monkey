207347e1995a51005e80c2ee73016bf44c589420
huanwei <huan@harmonycloud.cn>
2018-04-03 22:59:29 +0800

    upgrade victim client-go api to apps/v1

--- victims/factory/deployments/deployments_test.go
+++ victims/factory/deployments/deployments_test.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/asobti/kube-monkey/config"
 	"github.com/stretchr/testify/assert"
-	"k8s.io/api/extensions/v1beta1"
+	"k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -15,9 +15,9 @@ const (
 	NAMESPACE  = metav1.NamespaceDefault
 )
 
-func newDeployment(name string, labels map[string]string) v1beta1.Deployment {
+func newDeployment(name string, labels map[string]string) v1.Deployment {
 
-	return v1beta1.Deployment{
+	return v1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      name,
 			Namespace: NAMESPACE,
@@ -38,7 +38,7 @@ func TestNew(t *testing.T) {
 	depl, err := New(&v1depl)
 
 	assert.NoError(t, err)
-	assert.Equal(t, "v1beta1.Deployment", depl.Kind())
+	assert.Equal(t, "v1.Deployment", depl.Kind())
 	assert.Equal(t, NAME, depl.Name())
 	assert.Equal(t, NAMESPACE, depl.Namespace())
 	assert.Equal(t, IDENTIFIER, depl.Identifier())
