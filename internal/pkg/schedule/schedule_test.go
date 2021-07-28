4e2ada1c7bd63d52ebe490ffbb857df8e3ea7451
Daniel Albuquerque <dmartinsalbuqu@expediagroup.com>
2021-07-28 18:47:58 +0100

    idiomatic structure (#216)

---
+++ internal/pkg/schedule/schedule_test.go
@@ -0,0 +1,126 @@
+package schedule
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+	"time"
+
+	"kube-monkey/internal/pkg/chaos"
+	"kube-monkey/internal/pkg/config/param"
+
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+
+	"kube-monkey/internal/pkg/config"
+)
+
+func newSchedule() *Schedule {
+	return &Schedule{}
+}
+
+func TestEntries(t *testing.T) {
+	s := newSchedule()
+	assert.Equal(t, s.Entries(), s.entries)
+	assert.Len(t, s.Entries(), 0)
+}
+
+func TestAdd(t *testing.T) {
+	e := chaos.NewMock()
+	s := newSchedule()
+
+	s.Add(e)
+	assert.Len(t, s.entries, 1)
+
+}
+
+func TestStringNoEntries(t *testing.T) {
+	s := newSchedule()
+
+	schedString := []string{}
+	schedString = append(schedString, fmt.Sprint(Today))
+
+	schedString = append(schedString, fmt.Sprint(NoTermination))
+	schedString = append(schedString, fmt.Sprint(End))
+
+	assert.Equal(t, strings.Join(schedString, "\n"), s.String())
+}
+
+func TestStringNoEntriesWithID(t *testing.T) {
+
+	id := "TestingID"
+	os.Setenv("KUBE_MONKEY_ID", id)
+
+	s := newSchedule()
+
+	schedString := []string{}
+	schedString = append(schedString, fmt.Sprint(Today))
+	schedString = append(schedString, fmt.Sprintf(KubeMonkeyID, id))
+
+	schedString = append(schedString, fmt.Sprint(NoTermination))
+	schedString = append(schedString, fmt.Sprint(End))
+
+	assert.Equal(t, strings.Join(schedString, "\n"), s.String())
+
+	os.Unsetenv("KUBE_MONKEY_ID")
+}
+
+func TestStringWithEntries(t *testing.T) {
+	s := newSchedule()
+	e1 := chaos.NewMock()
+	e2 := chaos.NewMock()
+	s.Add(e1)
+	s.Add(e2)
+
+	schedString := []string{}
+	schedString = append(schedString, fmt.Sprint(Today))
+	schedString = append(schedString, fmt.Sprint(HeaderRow))
+	schedString = append(schedString, fmt.Sprint(SepRow))
+	for _, chaos := range s.entries {
+		schedString = append(schedString, fmt.Sprintf(RowFormat, chaos.Victim().Kind(), chaos.Victim().Namespace(), chaos.Victim().Name(), chaos.KillAt().Format(DateFormat)))
+	}
+	schedString = append(schedString, fmt.Sprint(End))
+
+	assert.Equal(t, strings.Join(schedString, "\n"), s.String())
+}
+
+func TestCalculateKillTimeRandom(t *testing.T) {
+	config.SetDefaults()
+	killtime := CalculateKillTime()
+
+	scheduledTime := func() (success bool) {
+		if killtime.Hour() >= config.StartHour() && killtime.Hour() <= config.EndHour() {
+			success = true
+		}
+		return
+	}
+
+	assert.Equal(t, killtime.Location(), config.Timezone())
+	assert.Condition(t, scheduledTime)
+
+}
+
+func TestCalculateKillTimeNow(t *testing.T) {
+	config.SetDefaults()
+	viper.SetDefault(param.DebugEnabled, true)
+	viper.SetDefault(param.DebugScheduleImmediateKill, true)
+	killtime := CalculateKillTime()
+
+	assert.Equal(t, killtime.Location(), config.Timezone())
+	assert.WithinDuration(t, killtime, time.Now(), time.Second*time.Duration(60))
+	config.SetDefaults()
+}
+
+func TestShouldScheduleChaosNow(t *testing.T) {
+	config.SetDefaults()
+	viper.SetDefault(param.DebugEnabled, true)
+	viper.SetDefault(param.DebugForceShouldKill, true)
+	assert.True(t, ShouldScheduleChaos(100000000000))
+	config.SetDefaults()
+}
+
+func TestShouldScheduleChaosMtbf(t *testing.T) {
+	assert.False(t, ShouldScheduleChaos(100000000000))
+	assert.True(t, ShouldScheduleChaos(1))
+}
---
+++ internal/pkg/victims/factory/factory.go
@@ -0,0 +1,86 @@
+/*
+Package factory is responsible for generating eligible victim kinds
+
+New types of kinds can be added easily
+*/
+package factory
+
+import (
+	"github.com/golang/glog"
+
+	"kube-monkey/internal/pkg/config"
+	"kube-monkey/internal/pkg/kubernetes"
+	"kube-monkey/internal/pkg/victims"
+	"kube-monkey/internal/pkg/victims/factory/daemonsets"
+	"kube-monkey/internal/pkg/victims/factory/deployments"
+	"kube-monkey/internal/pkg/victims/factory/statefulsets"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/selection"
+	"k8s.io/apimachinery/pkg/util/sets"
+)
+
+// EligibleVictims gathers list of enabled/enrolled kinds for judgement by
+// the scheduler
+// This checks against config.WhitelistedNamespaces but
+// each victim checks themselves against the ns blacklist
+// TODO: fetch all namespaces from k8 apiserver to check blacklist here
+func EligibleVictims() (eligibleVictims []victims.Victim, err error) {
+	clientset, err := kubernetes.CreateClient()
+	if err != nil {
+		return nil, err
+	}
+
+	// Verify opt-in at scheduling time
+	filter, err := enrollmentFilter()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, namespace := range config.WhitelistedNamespaces().UnsortedList() {
+		// Fetch deployments
+		deployments, err := deployments.EligibleDeployments(clientset, namespace, filter)
+		if err != nil {
+			//allow pass through to schedule other kinds and namespaces
+			glog.Warningf("Failed to fetch eligible deployments for namespace %s due to error: %s", namespace, err.Error())
+			continue
+		}
+		eligibleVictims = append(eligibleVictims, deployments...)
+
+		// Fetch statefulsets
+		statefulsets, err := statefulsets.EligibleStatefulSets(clientset, namespace, filter)
+		if err != nil {
+			//allow pass through to schedule other kinds and namespaces
+			glog.Warningf("Failed to fetch eligible statefulsets for namespace %s due to error: %s", namespace, err.Error())
+			continue
+		}
+		eligibleVictims = append(eligibleVictims, statefulsets...)
+
+		// Fetch daemonsets
+		daemonsets, err := daemonsets.EligibleDaemonSets(clientset, namespace, filter)
+		if err != nil {
+			//allow pass through to schedule other kinds and namespaces
+			glog.Warningf("Failed to fetch eligible daemonsets for namespace %s due to error: %s", namespace, err.Error())
+			continue
+		}
+		eligibleVictims = append(eligibleVictims, daemonsets...)
+	}
+
+	return
+}
+
+// Verifies opt-in of victims
+func enrollmentFilter() (*metav1.ListOptions, error) {
+	req, err := enrollmentRequirement()
+	if err != nil {
+		return nil, err
+	}
+	return &metav1.ListOptions{
+		LabelSelector: labels.NewSelector().Add(*req).String(),
+	}, nil
+}
+
+func enrollmentRequirement() (*labels.Requirement, error) {
+	return labels.NewRequirement(config.EnabledLabelKey, selection.Equals, sets.NewString(config.EnabledLabelValue).UnsortedList())
+}
